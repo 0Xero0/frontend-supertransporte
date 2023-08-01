f914df9860aee368730abe586a125f585eaecfde
Jesid Polo <jesing482@gmail.com>
2023-08-01 09:29:18 -0500

    https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
-  urlBackend: 'http://aplicaciones.supertransporte.gov.co:3333',
-  urlBackendVigia: 'http://aplicaciones.supertransporte.gov.co:3334',
-  urlBackendArchivos: 'http://aplicaciones.supertransporte.gov.co:4333',
+  urlBackend: 'https://aplicaciones.supertransporte.gov.co:3333',
+  urlBackendVigia: 'https://aplicaciones.supertransporte.gov.co:3334',
+  urlBackendArchivos: 'https://aplicaciones.supertransporte.gov.co:4333',
   tokenBackendArchivos: 'd4a32a3b-def6-4cc2-8f77-904a67360b53',
   llaveCaptcha: '6Ldw7FwmAAAAAJZyFVhvohzUI7mEK4wRPEqpGU9g'
 };
