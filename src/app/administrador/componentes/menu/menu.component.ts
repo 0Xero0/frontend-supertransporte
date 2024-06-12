9f84217c3b34e1947341516578d1d4ac17a51169
CésarM <61838637+0Xero0@users.noreply.github.com>
2024-06-12 18:15:05 -0500

    Cambio cerrar sesión

--- src/app/administrador/componentes/menu/menu.component.ts
+++ src/app/administrador/componentes/menu/menu.component.ts
@@ -15,12 +15,12 @@ export class MenuComponent implements OnInit {
   usuario?: Usuario | null;
   isCollapsed = false;
   desplegado = true
-  
+
   constructor(
-    private servicioLocalStorage: ServicioLocalStorage, 
+    private servicioLocalStorage: ServicioLocalStorage,
     private servicioAutenticacion: AutenticacionService,
     private router: Router
-  ) { 
+  ) {
   }
 
   ngOnInit(): void {
@@ -38,7 +38,9 @@ export class MenuComponent implements OnInit {
 
   public cerrarSesion(){
     this.servicioAutenticacion.cerrarSesion()
-    this.router.navigateByUrl('/inicio-sesion')
+    const ruta = 'http://localhost:4200/administrar/administrar-aplicativos'
+    location.href=ruta
+    /* this.router.navigateByUrl('') */
   }
   imprimirRuta(submodulo: Submodulo){
     console.log(`/administrar${submodulo.ruta}`)
