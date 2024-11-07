53354196ee31cb71a9ab4efe542206800cb2d110
Cesar <calfonsomora@gmail.com>
2024-11-07 15:25:17 -0500

    Cambio pa'tras.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -90,7 +90,7 @@ const routes: Routes = [
   {
     path: '',
     pathMatch: 'full',
-    redirectTo: 'inicio-sesion'
+    redirectTo: 'inicio-vigia2'
   },
   {
     path: 'inicio-vigia2',
@@ -115,7 +115,7 @@ const routes: Routes = [
   {
     path: '**',
     pathMatch: 'full',
-    redirectTo: 'inicio-sesion'
+    redirectTo: 'inicio-vigia2'
   }
 ];
 
